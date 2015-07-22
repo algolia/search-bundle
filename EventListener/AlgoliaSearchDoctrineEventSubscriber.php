682d161635bafc181c32b2bff150f74ea621fd13
Christophe BENOIT <christophe@kiwup.com>
2015-07-22 10:57:56 +0200

    added try / catch with logging in doctrine subscriber to prevent errors bubbling up (#13)

--- EventListener/AlgoliaSearchDoctrineEventSubscriber.php
+++ EventListener/AlgoliaSearchDoctrineEventSubscriber.php
@@ -1,22 +1,26 @@
 <?php
 namespace Algolia\AlgoliaSearchBundle\EventListener;
 
+use AlgoliaSearch\AlgoliaException;
 use Doctrine\Common\EventSubscriber;
 use Doctrine\ORM\Event\OnFlushEventArgs;
 
 use Algolia\AlgoliaSearchBundle\Indexer\Indexer;
+use Psr\Log\LoggerInterface;
 
 class AlgoliaSearchDoctrineEventSubscriber implements EventSubscriber
 {
     private $indexer;
+    private $logger;
 
     /**
      * Under normal circumstances, the service loader will set the indexer.
      * @param Indexer $indexer
      */
-    public function __construct(Indexer $indexer)
+    public function __construct(Indexer $indexer, LoggerInterface $logger)
     {
         $this->indexer = $indexer;
+        $this->logger = $logger;
     }
 
     /**
@@ -40,49 +44,55 @@ class AlgoliaSearchDoctrineEventSubscriber implements EventSubscriber
      */
     public function onFlush(OnFlushEventArgs $args)
     {
-        /**
-         * There might have been an exception thrown during the previous flush attempt,
-         * because the DB rejected our changes for instance.
-         * We clean our indexer cache to prevent double indexing stuff if this happened.
-         */
-        $this->indexer->removeScheduledIndexChanges();
+        try {
+            /**
+             * There might have been an exception thrown during the previous flush attempt,
+             * because the DB rejected our changes for instance.
+             * We clean our indexer cache to prevent double indexing stuff if this happened.
+             */
+            $this->indexer->removeScheduledIndexChanges();
 
-        $em = $args->getEntityManager();
-        $uow = $em->getUnitOfWork();
+            $em = $args->getEntityManager();
+            $uow = $em->getUnitOfWork();
 
-        foreach ($uow->getScheduledEntityInsertions() as $entity) {
-            if ($this->indexer->autoIndex($entity, $em)) {
-                $this->create($entity);
+            foreach ($uow->getScheduledEntityInsertions() as $entity) {
+                if ($this->indexer->autoIndex($entity, $em)) {
+                    $this->create($entity);
+                }
             }
-        }
 
-        foreach ($uow->getScheduledEntityUpdates() as $entity) {
-            if ($this->indexer->autoIndex($entity, $em)) {
-                $changeSet = $uow->getEntityChangeSet($entity);
-                $this->update($entity, $changeSet);
+            foreach ($uow->getScheduledEntityUpdates() as $entity) {
+                if ($this->indexer->autoIndex($entity, $em)) {
+                    $changeSet = $uow->getEntityChangeSet($entity);
+                    $this->update($entity, $changeSet);
+                }
             }
-        }
 
-        foreach ($uow->getScheduledEntityDeletions() as $entity) {
-            if ($this->indexer->autoIndex($entity, $em)) {
-                $originalData = $uow->getOriginalEntityData($entity);
-                $this->delete($entity, $originalData);
+            foreach ($uow->getScheduledEntityDeletions() as $entity) {
+                if ($this->indexer->autoIndex($entity, $em)) {
+                    $originalData = $uow->getOriginalEntityData($entity);
+                    $this->delete($entity, $originalData);
+                }
             }
-        }
 
-        /**
-         * There are also:
-         *
-         * $uow->getScheduledCollectionDeletions();
-         * $uow->getScheduledCollectionUpdates();
-         *
-         * But they're not relevant here, I think.
-         *
-         * Apparently they're used for internal bookkeeping when
-         * doing things with Many-To-Many relationships.
-         *
-         * Leaving the comment just in case I'm wrong.
-         */
+            /**
+             * There are also:
+             *
+             * $uow->getScheduledCollectionDeletions();
+             * $uow->getScheduledCollectionUpdates();
+             *
+             * But they're not relevant here, I think.
+             *
+             * Apparently they're used for internal bookkeeping when
+             * doing things with Many-To-Many relationships.
+             *
+             * Leaving the comment just in case I'm wrong.
+             */
+        } catch (AlgoliaException $e) {
+            $this->logger->error('Algolia: '.$e->getMessage());
+        } catch (\Exception $e) {
+            $this->logger->error($e->getMessage());
+        }
     }
 
     /**
@@ -90,7 +100,13 @@ class AlgoliaSearchDoctrineEventSubscriber implements EventSubscriber
      */
     public function postFlush($ignoredArgument)
     {
-        $this->indexer->processScheduledIndexChanges();
+        try {
+            $this->indexer->processScheduledIndexChanges();
+        } catch (AlgoliaException $e) {
+            $this->logger->error('Algolia: '.$e->getMessage());
+        } catch (\Exception $e) {
+            $this->logger->error($e->getMessage());
+        }
     }
 
     protected function create($entity)
