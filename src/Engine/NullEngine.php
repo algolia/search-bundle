7141820f530726ae6b63d082879baf630c91269d
Julien Bourdeau <julien@sigerr.org>
2017-11-28 14:26:20 +0100

    feat(engine): Move NullEngine from tests to src

--- src/Engine/NullEngine.php
+++ src/Engine/NullEngine.php
@@ -8,41 +8,41 @@ class NullEngine implements EngineInterface
 
     public function add($searchableEntities)
     {
-        // TODO: Implement add() method.
+        //
     }
 
     public function update($searchableEntities)
     {
-        // TODO: Implement update() method.
+        //
     }
 
     public function remove($searchableEntities)
     {
-        // TODO: Implement delete() method.
+        //
     }
 
     public function clear($indexName)
     {
-        // TODO: Implement clear() method.
+        //
     }
 
     public function delete($indexName)
     {
-        // TODO: Implement delete() method.
+        //
     }
 
     public function search($query, $indexName, $page = 0, $nbResults = null, array $parameters = [])
     {
-        // TODO: Implement search() method.
+        return [];
     }
 
     public function searchIds($query, $indexName, $page = 0, $nbResults = null, array $parameters = [])
     {
-        // TODO: Implement searchIds() method.
+        return [];
     }
 
     public function count($query, $indexName)
     {
-        // TODO: Implement count() method.
+        return 0;
     }
 }
