e710b8d853ea7b874086cbcb049031a548edfbd5
Julien Bourdeau <julien@sigerr.org>
2017-12-15 12:18:37 +0100

    chore(test): Add travis configuration

--- tests/AlgoliaSearch/SettingsTest.php
+++ tests/AlgoliaSearch/SettingsTest.php
@@ -23,7 +23,7 @@ class SettingsTest extends BaseTest
         $this->client = $this->container->get('algolia.client');
 
         $this->settingsManager = new AlgoliaSettingsManager($this->client, [
-            'prefix' => 'sf4_bundle_testing_',
+            'prefix' => 'TRAVIS_sf_settings_',
             'settingsDirectory' => $this->settingsDir,
             'indices' => [
                 'posts' => [
@@ -40,17 +40,17 @@ class SettingsTest extends BaseTest
             'hitsPerPage' => 51,
             'maxValuesPerFacet' => 99,
         ];
-        $index = $this->client->initIndex('sf4_bundle_testing_posts');
+        $index = $this->client->initIndex('TRAVIS_sf_settings_posts');
         $task = $index->setSettings($settingsToUpdate);
         $index->waitTask($task['taskID']);
 
         $message = $this->settingsManager->backup(['indices' => ['posts']]);
 
         $this->assertContains('Saved settings for', $message[0]);
-        $this->assertTrue(file_exists($this->settingsDir.'/sf4_bundle_testing_posts-settings.json'));
+        $this->assertTrue(file_exists($this->settingsDir.'/TRAVIS_sf_settings_posts-settings.json'));
 
         $savedSettings = json_decode(file_get_contents(
-            $this->settingsDir.'/sf4_bundle_testing_posts-settings.json'
+            $this->settingsDir.'/TRAVIS_sf_settings_posts-settings.json'
         ), true);
 
         $this->assertArraySubset($settingsToUpdate, $savedSettings);
@@ -65,7 +65,7 @@ class SettingsTest extends BaseTest
             'hitsPerPage' => 12,
             'maxValuesPerFacet' => 100,
         ];
-        $index = $this->client->initIndex('sf4_bundle_testing_posts');
+        $index = $this->client->initIndex('TRAVIS_sf_settings_posts');
         $task = $index->setSettings($settingsToUpdate);
         $index->waitTask($task['taskID']);
 
@@ -74,7 +74,7 @@ class SettingsTest extends BaseTest
         $this->assertContains('Pushed settings for', $message[0]);
 
         $savedSettings = json_decode(file_get_contents(
-            $this->settingsDir.'/sf4_bundle_testing_posts-settings.json'
+            $this->settingsDir.'/TRAVIS_sf_settings_posts-settings.json'
         ), true);
 
         for ($i=0;$i<5;$i++) {
