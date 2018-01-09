c5dc9139545a8636d51536c46f3fb77fb90bbc66
Julien Bourdeau <julien@sigerr.org>
2018-01-09 11:00:14 +0100

    Add a compiler pass for checking requirements

--- src/AlgoliaSearchBundle.php
+++ src/AlgoliaSearchBundle.php
@@ -3,6 +3,8 @@
 namespace Algolia\SearchBundle;
 
 use AlgoliaSearch\Version;
+use Algolia\SearchBundle\DependencyInjection\SearchRequirementsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\HttpKernel\Kernel as SfKernel;
 
@@ -14,5 +16,12 @@ class AlgoliaSearchBundle extends Bundle
 
         Version::addSuffixUserAgentSegment('Symfony', SfKernel::VERSION);
         Version::addSuffixUserAgentSegment('Symfony Search Bundle', '3.0.0-BETA');
+    }
+    
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new SearchRequirementsPass());
     }
 }
