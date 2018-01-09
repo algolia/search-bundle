8727ad6cafac6f2dadf06c8a6e5d1c49df1019bc
Julien Bourdeau <julien@sigerr.org>
2018-01-09 11:14:01 +0100

    Move UA definition to Kernel::boot and add Sf version

--- src/AlgoliaSearchBundle.php
+++ src/AlgoliaSearchBundle.php
@@ -2,8 +2,17 @@
 
 namespace Algolia\SearchBundle;
 
+use AlgoliaSearch\Version;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\HttpKernel\Kernel as SfKernel;
 
 class AlgoliaSearchBundle extends Bundle
 {
+    public function boot()
+    {
+        parent::boot();
+
+        Version::addSuffixUserAgentSegment('Symfony', SfKernel::VERSION);
+        Version::addSuffixUserAgentSegment('Symfony Search Bundle', '3.0.0-BETA');
+    }
 }
