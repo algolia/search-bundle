4acebdfbc1c3603d1b36bdbf84059f5e0829e8bd
Julien Bourdeau <julien@sigerr.org>
2018-01-10 11:42:07 +0100

    fix(commands): Container is necessary for doctrine

--- src/Command/IndexCommand.php
+++ src/Command/IndexCommand.php
@@ -4,11 +4,11 @@ namespace Algolia\SearchBundle\Command;
 
 
 use Algolia\SearchBundle\IndexManagerInterface;
-use Symfony\Component\Console\Command\Command;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-abstract class IndexCommand extends Command
+abstract class IndexCommand extends ContainerAwareCommand
 {
     protected $indexManager;
 
