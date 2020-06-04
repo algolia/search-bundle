e01f080900698151dfeb3a99e9d20ab05617f4af
jesko-plitt <54807545+jesko-plitt@users.noreply.github.com>
2020-06-04 11:35:55 +0200

    Add `bool` type to `$requestOptions` array

--- src/SearchService.php
+++ src/SearchService.php
@@ -34,41 +34,41 @@ interface SearchService
     public function searchableAs($className);
 
     /**
-     * @param object|array<int, object>                      $searchables
-     * @param array<string, int|string|array>|RequestOptions $requestOptions
+     * @param object|array<int, object>                           $searchables
+     * @param array<string, int|string|bool|array>|RequestOptions $requestOptions
      *
      * @return \Algolia\AlgoliaSearch\Response\AbstractResponse
      */
     public function index(ObjectManager $objectManager, $searchables, $requestOptions = []);
 
     /**
-     * @param object|array<int, object>                      $searchables
-     * @param array<string, int|string|array>|RequestOptions $requestOptions
+     * @param object|array<int, object>                           $searchables
+     * @param array<string, int|string|bool|array>|RequestOptions $requestOptions
      *
      * @return \Algolia\AlgoliaSearch\Response\AbstractResponse
      */
     public function remove(ObjectManager $objectManager, $searchables, $requestOptions = []);
 
     /**
-     * @param string                                         $className
-     * @param array<string, int|string|array>|RequestOptions $requestOptions
+     * @param string                                              $className
+     * @param array<string, int|string|bool|array>|RequestOptions $requestOptions
      *
      * @return \Algolia\AlgoliaSearch\Response\AbstractResponse
      */
     public function clear($className, $requestOptions = []);
 
     /**
-     * @param string                                         $className
-     * @param array<string, int|string|array>|RequestOptions $requestOptions
+     * @param string                                              $className
+     * @param array<string, int|string|bool|array>|RequestOptions $requestOptions
      *
      * @return \Algolia\AlgoliaSearch\Response\AbstractResponse
      */
     public function delete($className, $requestOptions = []);
 
     /**
-     * @param string                                         $className
-     * @param string                                         $query
-     * @param array<string, int|string|array>|RequestOptions $requestOptions
+     * @param string                                              $className
+     * @param string                                              $query
+     * @param array<string, int|string|bool|array>|RequestOptions $requestOptions
      *
      * @return array<int, object>
      *
@@ -77,9 +77,9 @@ interface SearchService
     public function search(ObjectManager $objectManager, $className, $query = '', $requestOptions = []);
 
     /**
-     * @param string                                         $className
-     * @param string                                         $query
-     * @param array<string, int|string|array>|RequestOptions $requestOptions
+     * @param string                                              $className
+     * @param string                                              $query
+     * @param array<string, int|string|bool|array>|RequestOptions $requestOptions
      *
      * @return array<string, int|string|bool|array>
      *
@@ -88,9 +88,9 @@ interface SearchService
     public function rawSearch($className, $query = '', $requestOptions = []);
 
     /**
-     * @param string                                         $className
-     * @param string                                         $query
-     * @param array<string, int|string|array>|RequestOptions $requestOptions
+     * @param string                                              $className
+     * @param string                                              $query
+     * @param array<string, int|string|array|bool>|RequestOptions $requestOptions
      *
      * @return int
      *
