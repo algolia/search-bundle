b96024e540c59d4c6d7790a5d474ca6abb3ed4b6
Devin Beeuwkes <devin.beeuwkes@algolia.com>
2021-12-20 15:30:04 +0100

    Update Aggregator.php

--- src/Model/Aggregator.php
+++ src/Model/Aggregator.php
@@ -85,7 +85,18 @@ abstract class Aggregator implements NormalizableInterface
     }
 
     /**
-     * {@inheritdoc}
+     * Normalizes the object into an array of scalars|arrays.
+     *
+     * It is important to understand that the normalize() call should normalize
+     * recursively all child objects of the implementor.
+     *
+     * @param NormalizerInterface $normalizer The normalizer is given so that you
+     *                                        can use it to normalize objects contained within this object
+     * @param string|null         $format     The format is optionally given to be able to normalize differently
+     *                                        based on different output formats
+     * @param array               $context    Options for normalizing this object
+     *
+     * @return array|string|int|float|bool
      */
     public function normalize(NormalizerInterface $normalizer, $format = null, array $context = [])
     {
